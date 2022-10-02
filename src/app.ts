d0ce849cd0bab48ad2811e69a2b5d97cc5a33dc2
Klaus Kupper <klausdk1999@gmail.com>
2022-10-01 22:29:11 -0300

    patch: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { json } from 'express';
 import 'express-async-errors';
-
 import errorHandler from './middlewares/errorHandlerMiddleware';
 import router from './routes/index';
+import cors from 'cors';
 
 const app = express();
 app.use(json());
+app.use(cors())
 app.use(router);
 app.use(errorHandler);
 
